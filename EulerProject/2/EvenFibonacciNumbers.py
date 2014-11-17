1ef94c1ea11b0485ccf5b4ff6d39a94eb72dd914
pawlmurray <pawlmurray@gmail.com>
2014-11-17 18:40:48 -0500

    Added a readme for the challenging problem #3

---
+++ EulerProject/2/EvenFibonacciNumbers.py
@@ -0,0 +1,14 @@
+def fibonacchiEvenSum(max):
+	prev = 1
+	currentSum = 0
+	currentValue = 2
+	while currentValue < max:
+		if currentValue % 2 == 0:
+			currentSum += currentValue
+		oldValue = currentValue
+		currentValue += prev
+		prev = oldValue
+	return currentSum
+
+
+print fibonacchiEvenSum(4000000)
