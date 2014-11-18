01e10f55bd2c93c3fe5a71d5d1c2059632a76cfa
pawlmurray <pawlmurray@gmail.com>
2014-11-17 19:49:03 -0500

    Hopefully fixed the tab issue

--- EulerProject/1/MultiplesOf3sAnd5s.py
+++ EulerProject/1/MultiplesOf3sAnd5s.py
@@ -1,23 +1,24 @@
 
 #get all of the multiples of threes and add them up
 def sumOfMultiplesOfThrees(max):
-	currentSum = 0
-	threeCounter = 3
-	while(threeCounter < max):
-		currentSum+= threeCounter
-		threeCounter += 3
-	return currentSum
+  currentSum = 0
+  threeCounter = 3
+  while(threeCounter < max):
+    currentSum+= threeCounter
+    threeCounter += 3
+  return currentSum
 
 #Get all of the multiples of fives and add them up, if they are
 #evenly divisible by three just leave them out
 def sumOfMultiplesOfFivesWithoutThrees(max):
-	currentSum = 0
-	fiveCounter = 5
-	while(fiveCounter < max):
-		if not fiveCounter % 3 == 0:
-			currentSum += fiveCounter
-		fiveCounter += 5
-	return currentSum
+  currentSum = 0
+  fiveCounter = 5
+  while(fiveCounter < max):
+    if not fiveCounter % 3 == 0:
+      currentSum += fiveCounter
+    fiveCounter += 5
+  return currentSum
 
 totalSum = sumOfMultiplesOfThrees(1000) + sumOfMultiplesOfFivesWithoutThrees(1000)
 print totalSum
+    
