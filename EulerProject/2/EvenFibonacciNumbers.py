c82c962e2f82e8026e69c77d8119f8f36b48c394
pawlmurray <pawlmurray@gmail.com>
2014-11-17 19:57:30 -0500

    updated the spacing on 2-4

--- EulerProject/2/EvenFibonacciNumbers.py
+++ EulerProject/2/EvenFibonacciNumbers.py
@@ -1,14 +1,13 @@
 def fibonacchiEvenSum(max):
-	prev = 1
-	currentSum = 0
-	currentValue = 2
-	while currentValue < max:
-		if currentValue % 2 == 0:
-			currentSum += currentValue
-		oldValue = currentValue
-		currentValue += prev
-		prev = oldValue
-	return currentSum
-
+    prev = 1
+    currentSum = 0
+    currentValue = 2
+    while currentValue < max:
+        if currentValue % 2 == 0:
+            currentSum += currentValue
+        oldValue = currentValue
+        currentValue += prev
+        prev = oldValue
+    return currentSum
 
 print fibonacchiEvenSum(4000000)
--- EulerProject/4/LargestPalindromeProduct.py
+++ EulerProject/4/LargestPalindromeProduct.py
@@ -1,32 +1,32 @@
 #returns true if the number is a palindrome
 def isPalindrome(num):
-	numAsStr = str(num)
-	for index in range (0, len(numAsStr) / 2):
-		if not numAsStr[index] == numAsStr[-(index+1)]:
-			return False
-	return True
+    numAsStr = str(num)
+    for index in range (0, len(numAsStr) / 2):
+        if not numAsStr[index] == numAsStr[-(index+1)]:
+            return False
+    return True
 
 
 #given one number, find the largest palindrome that number can make with another triple digit number
 def findLargestPalindromeAndOtherFactor(num1):
-	num2 = 999
-	hasFoundPalindrome = False
-	while num2 > 99:
-		if isPalindrome(num1 * num2):
-			return num1 * num2
-		num2-=1
-	return -1
+    num2 = 999
+    hasFoundPalindrome = False
+    while num2 > 99:
+        if isPalindrome(num1 * num2):
+            return num1 * num2
+        num2-=1
+    return -1
 
-#finds the largest triple digit palindrome
+    #finds the largest triple digit palindrome
 def findLargestTripleDigitPalindrome():
-	numToTry = 999
-	biggestPalindrome = -1
-	while numToTry > 99:
-		palindromeAttempt = findLargestPalindromeAndOtherFactor(numToTry)
-		if not palindromeAttempt == -1 and palindromeAttempt > biggestPalindrome:
-			biggestPalindrome = palindromeAttempt
-		numToTry-=1
-	return biggestPalindrome  
+    numToTry = 999
+    biggestPalindrome = -1
+    while numToTry > 99:
+        palindromeAttempt = findLargestPalindromeAndOtherFactor(numToTry)
+        if not palindromeAttempt == -1 and palindromeAttempt > biggestPalindrome:
+            biggestPalindrome = palindromeAttempt
+        numToTry-=1
+    return biggestPalindrome  
 
 #def findLargest
 
