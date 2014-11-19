c4bfcb9fdb794e812053541f59b5d3ee5987e916
pawlmurray <pawlmurray@gmail.com>
2014-11-19 18:52:33 -0500

    Well problem 10 was not easy

---
+++ ProjectEuler/10/SummationOfPrimes.py
@@ -0,0 +1,32 @@
+#Cool way of calculating all of the primes less then n.
+#Essentailly we make a blank dictionary with keys being
+#the number and values representing whether or not it is prime.
+#After that we iterate through looking for a number that is still identified
+#as prime. Once we discover a prime we continually multiply that number
+#by increasing multiples (starting at a multiple of itself), marking those
+#products as not prime. 
+def sieveOfEratostehnes(n):
+    primes = [True] * (n + 1)
+    primes[0] = False
+    primes[1] = False
+    for p in range(2, n + 1):
+        if primes[p] == True:
+            currentMult = p
+            currentNum = p * currentMult
+            while currentNum < n + 1:
+                primes[currentNum] = False
+                currentMult += 1
+                currentNum = currentMult * p
+    return primes
+
+#just add up all of the numbers that are marked true
+def calculateSummationOfPrimesLessThenN(n):
+    primes = sieveOfEratostehnes(n)
+    currentSum = 0
+    for index in range(0, len(primes)):
+        if primes[index]:
+            currentSum += index
+    return currentSum
+
+
+print calculateSummationOfPrimesLessThenN(2000000)
